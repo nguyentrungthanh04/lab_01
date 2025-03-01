a2fd080dbfa75b5c041c1475570ca4cea90c978d
nguyentrungthanh04 <seongtrungthanh@gmail.com>
2025-03-01 14:20:52 +0700

    Add files via upload

---
+++ lab_01/README.md
@@ -0,0 +1,2 @@
+# lab_01
+Bài tập thực hành bảo mật nâng cao
---
+++ lab_01/ex02_04.py
@@ -0,0 +1,5 @@
+ds = []
+for i in range (2000, 3201):
+    if (i % 7 == 0) and (i % 5 != 0):
+        ds.append(str(i))
+print(','.join(ds))
